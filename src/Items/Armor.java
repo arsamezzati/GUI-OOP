16f5252eaa9776bb77171ede3ef92a37cd0dfda4
hila <hilakargar@gmail.com>
2023-11-12 18:30:44 +0100

    ItemStats Class deleted. Armor Class edited. Weapon Class edited.

--- src/Items/Armor.java
+++ src/Items/Armor.java
@@ -1,9 +1,23 @@
 package Items;
 
-public class Armor<T> extends Item{
+import java.util.Random;
+
+public class Armor extends Item{
     public  Armor(String name,int health){
         super(name);
         this.setHealth(health);
         this.setAttackDamage(0);
+    }
+    private static final String[] armorList = {"Bronze Armor","Silver Armor","Gold Armor"};
+
+    public Armor handleTreasureEvent(){
+        return generateArmor();
+    }
+
+    private Armor generateArmor(){
+        Random random = new Random();
+        int r = random.nextInt(armorList.length);
+        return new Armor(armorList[r],150);
+
     }
 }
